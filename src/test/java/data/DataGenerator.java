da5fa12bda22585d584f40e6f6c9c40b5bbabc20
Mel66292 <mel57792@yandex.ru>
2023-08-05 21:16:09 +0500

    First commit

---
+++ src/test/java/data/DataGenerator.java
@@ -0,0 +1,68 @@
+package data;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+import java.util.Locale;
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    private static final Faker faker = new Faker(new Locale("eng"));
+
+    private DataGenerator() {}
+
+    public static void sendRequest(RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+
+    public static String getLogin() {
+        String login = faker.internet().emailAddress();
+        return login;
+    }
+
+    public static String getPassword() {
+        String password = faker.internet().password();
+        return password;
+    }
+
+    public static class Registration {
+
+        private Registration() {}
+
+        public static RegistrationDto getUser(String status) {
+            var user = new RegistrationDto(getLogin(), getPassword(), status);
+            return user;
+        }
+        public static RegistrationDto getRegisteredUser(String status) {
+            var registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
+
---
+++ src/test/java/test/JSONTest.java
@@ -0,0 +1,78 @@
+package test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import static com.codeborne.selenide.Selenide.*;
+import static data.DataGenerator.Registration.getRegisteredUser;
+import static data.DataGenerator.Registration.getUser;
+
+public class JSONTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    @DisplayName("should test with valid registered active user")
+    void shouldTestWithValidRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    @DisplayName("should test with valid registered blocked user")
+    void shouldTestWithValidRegisteredBlockedUser() {
+        var registeredUser = getRegisteredUser("blocked");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id=error-notification] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Пользователь заблокирован"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(5));
+    }
+
+    @Test
+    @DisplayName("should test with valid unregistered user")
+    void shouldTestWithValidUnregisteredUser() {
+        var unregisteredUser = getUser("active");
+        $("[data-test-id=login] input").setValue(unregisteredUser.getLogin());
+        $("[data-test-id=password] input").setValue(unregisteredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id=error-notification] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(5));
+    }
+
+    @Test
+    @DisplayName("should test with registered user wrong login")
+    void shouldTestWithValidRegisteredUserWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var unregisteredUser = getUser("active");
+        $("[data-test-id=login] input").setValue(unregisteredUser.getLogin());
+        $("[data-test-id=password] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id=error-notification] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(5));
+    }
+
+    @Test
+    @DisplayName("should test with registered user wrong pass")
+    void shouldTestWithValidRegisteredUserWrongPass() {
+        var registeredUser = getRegisteredUser("active");
+        var unregisteredUser = getUser("active");
+        $("[data-test-id=login] input").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] input").setValue(unregisteredUser.getPassword());
+        $("button.button").click();
+        $("[data-test-id=error-notification] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(5));
+    }
+}
